f928d133bec081669f3d1304894520567a8ca5ad
Andres Cuevas <acg.hermesmusic@gmail.com>
2023-10-10 11:45:30 -0500

    ftr: Agregando componente de entrada

--- src/components/entrada/Entrada.jsx
+++ src/components/entrada/Entrada.jsx
@@ -1,10 +1,17 @@
 import "./entrada.css";
-const Entrada = () => {
+const Entrada = ({ entrada, handle, handleEx }) => {
   return (
-    <form className="form__entrada">
+    <form className="form__entrada" onSubmit={handleEx}>
       <label className="container__input">
         <span className="input__label">Ingresa un numero:</span>
-        <input type="text" className="input__in" />
+        <input
+          type="text"
+          className="input__in"
+          name="entrada"
+          id="entrada"
+          value={entrada}
+          onChange={handle}
+        />
       </label>
       <button className="entrada__btn">Ejecutar</button>
     </form>
